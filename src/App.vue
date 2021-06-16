e8b93a0473f24931d9c04cea5cc51044a37f7dc1
Vu Nguyen <nguyenhvubk@gmail.com>
2021-06-16 23:06:48 +0700

    add git ignore

--- src/App.vue
+++ src/App.vue
@@ -27,12 +27,12 @@ body {
   margin: 0;
   background-color: #f7fcff !important;
 }
-#app {
+/* #app { */
   /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale; */
   /* text-align: center; */
   /* color: #2c3e50; */
   /* margin-top: 30px; */
-}
+/* } */
 </style>
